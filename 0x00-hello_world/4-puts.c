3455d1384d10ac1fa494af701475b00d9ada14cd
amanu251 <amanmenge251@gmail.com>
2022-09-08 23:26:25 +0000

    fix coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
+ /**
  * main - Entry point
  *
  * return: Always 0 (success)
  */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
-}
+ int main(void)
+ { 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+ }
