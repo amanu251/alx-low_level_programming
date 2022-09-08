086882ecccd711b945b8dbaeae520199f1d5174c
amanu251 <amanmenge251@gmail.com>
2022-09-08 23:28:51 +0000

    fix spacing and white space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
- * main - Entry point
- *
- * return: Always 0 (success)
- */
- int main(void)
- { 
+/**
+*main - Entry point
+*
+*return: Always 0 (success)
+*/
+int main(void)
+{ 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- }
+}
