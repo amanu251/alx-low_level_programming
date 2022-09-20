98d870057ccdc143d6baa65efc1a1d673f943dfb
amanu251 <amanmenge251@gmail.com>
2022-09-20 01:15:44 +0000

    changing variable value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset integer to 98
+ *@n :pointer dereferencing
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
