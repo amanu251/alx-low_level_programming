335c1f5c55d53bc0df93c8ed792c975d10c7a2be
amanu251 <amanmenge251@gmail.com>
2022-09-20 01:51:14 +0000

    string size

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string
+ *
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
