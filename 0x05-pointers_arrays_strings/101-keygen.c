5e00904eda56660029e6f4e63114e41976b6295a
amanu251 <amanmenge251@gmail.com>
2022-09-21 02:38:58 +0000

    random paasowrd .

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entrance point: generates a random password
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+	int i, n, pass, pd;
+
+	char values[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char password[222];
+
+	srand(time(0));
+	for (i = 0; pass < 2772; i++)
+	{
+		n = rand() % 10;
+		password[i] = values[n];
+		pass += password[i];
+	}
+	pd = 2772 - pass;
+	password[i] = pd;
+	printf("%s\n", password);
+
+	return (0);
+}
