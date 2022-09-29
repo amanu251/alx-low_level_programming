7f9dcc48bf1acf7dda1acbcf2d2194f001c708c2
amanu251 <amanmenge251@gmail.com>
2022-09-29 19:35:05 +0000

    last tasks

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print the power of a number
+ * @x: input integer
+ * @y: input integer
+ *
+ * Return: an integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
