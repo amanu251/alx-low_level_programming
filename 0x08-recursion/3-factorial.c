0d0f0098fffc9318965469b0cfec56fb73b97bd1
amanu251 <amanmenge251@gmail.com>
2022-09-29 12:12:43 +0000

    I betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,17 +4,17 @@
  * factorial - calculate factorial of an integer
  * @n: input integer
  *
- * Return: an integer 
+ * Return: an integer
  */
 int factorial(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	if(n <=1)
+	if (n <= 1)
 	{
 		return (1);
 	}
-	return ( n * factorial(n - 1));	
+	return (n * factorial(n - 1));
 }
