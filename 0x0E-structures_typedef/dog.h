0b54fbfd1dc575cefbfc0f84fd86207d81e2f384
amanu251 <amanmenge251@gmail.com>
2022-10-11 01:47:14 +0000

    woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h_
+#define dog_h_
+
+/**
+ * struct dog - data type including dog info
+ * @name: the dogs name
+ * @age: age of dog
+ * @owner: name of dogs owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
