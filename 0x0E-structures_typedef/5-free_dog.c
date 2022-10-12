ccb9fc9f3d93776269484a7dd938b195fd250cc8
amanu251 <amanmenge251@gmail.com>
2022-10-12 02:30:52 +0000

    free last task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog -free the memory allocated to dog_t
+ * @d: struct pointer
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
